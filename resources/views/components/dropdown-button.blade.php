4e4c18e35ae497f3f2275262eb7640f2b8f4169c
Rowshon Habib Rony <rhrony0009@gmail.com>
2024-02-29 01:49:22 +0600

    bug fix

--- resources/views/components/dropdown-button.blade.php
+++ resources/views/components/dropdown-button.blade.php
@@ -1,5 +1,5 @@
 <li>
-    <button {{ $attributes->merge(['class' => 'dropdown-item']) }} @if ($id != '') data-id="{{ $id }}" @endif>
+    <button {{ $attributes->merge(['class' => 'dropdown-item']) }}>
         @if ($icon != '')
             <i class="fa fa-{{ $icon }}"></i>
         @endif {{ $slot }}
