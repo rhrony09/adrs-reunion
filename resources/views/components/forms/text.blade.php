7e563ac86a8335bfaeaa2d4102ed3c3d8ac7837f
Rowshon Habib Rony <rhrony0009@gmail.com>
2024-02-29 01:54:58 +0600

    aa

--- resources/views/components/forms/text.blade.php
+++ resources/views/components/forms/text.blade.php
@@ -1,7 +1,4 @@
 <div {{ $attributes->merge(['class' => 'mt-3']) }}>
     <x-forms.label :fieldId="$fieldId" :fieldLabel="$fieldLabel" :fieldRequired="$fieldRequired"></x-forms.label>
     <input type="text" class="form-control" value="{{ $fieldValue }}" name="{{ $fieldName }}" id="{{ $fieldId }}" @if ($fieldRequired == 'true') required @endif>
-    @if ($fieldHelp)
-        <small id="{{ $fieldId }}Help" class="form-text text-muted">{{ $fieldHelp }}</small>
-    @endif
 </div>
