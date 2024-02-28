3f2ccda8143dc06bad916cd082e91c5f78eedb34
Rowshon Habib Rony <rhrony0009@gmail.com>
2024-02-29 01:51:32 +0600

    bug fix

--- resources/views/components/forms/label.blade.php
+++ resources/views/components/forms/label.blade.php
@@ -2,8 +2,4 @@
     @if ($fieldRequired == 'true')
         <sup class="mr-1">*</sup>
     @endif
-
-    @if (!is_null($popover))
-        <i class="fa fa-question-circle" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="{{ $popover }}"></i>
-    @endif
 </label>
